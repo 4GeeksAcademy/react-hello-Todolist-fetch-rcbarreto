2e3693de20113808d7e8f0e1147299475f263254
rcbarreto <69176511+rcbarreto@users.noreply.github.com>
2024-10-18 01:06:42 +0000

    Logrado eliminar elementos de la lista

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,51 @@
-import React from "react";
+import React, {useState} from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
+import { isElementOfType } from "react-dom/test-utils";
 
 //create your first component
 const Home = () => {
+
+	let [listaDeTareas, setListaDeTareas] = useState(["bañarse", "limpiar","cocinar","aprender react"])
+	
+	const eliminarTarea = (index) => {
+        const nuevaLista = listaDeTareas.filter((_, i) => i !== index);
+        setListaDeTareas(nuevaLista); 
+    };
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		
+		<div className="container mt-5">
+			
+			<h1 className="text-center mt-5">Todos</h1>
+
+			<div className="mx-auto col-6">
+		
+				<input type="email" className="form-control" placeholder=" ¿Que necesitas hacer?" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+				
+				<ul className="">
+										
+						{listaDeTareas.map((item, index) => {
+							
+							return (
+
+								<li key = {index}>	
+									{item}	<i className="fa-solid fa-trash hideIcon" onClick={()=> eliminarTarea(index)}></i>
+								</li>
+							)
+
+							})} 
+					
+				</ul>
+		
+			</div>
+	
 		</div>
+		
+			
+			
+		
 	);
 };
 
