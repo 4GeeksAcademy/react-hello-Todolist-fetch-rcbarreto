13cce0e82d1ed925c3e0b4f19318f5b379958b6d
rcbarreto <69176511+rcbarreto@users.noreply.github.com>
2024-10-20 03:15:01 +0000

    Terminado

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -7,7 +7,7 @@ import { isElementOfType } from "react-dom/test-utils";
 //create your first component
 const Home = () => {
 
-	let [listaDeTareas, setListaDeTareas] = useState(["bañarse", "limpiar","cocinar","aprender react"])
+	let [listaDeTareas, setListaDeTareas] = useState(["Bañarse", "Limpiar","Cocinar","Aprender react"])
 	
 	const eliminarTarea = (index) => {
         const nuevaLista = listaDeTareas.filter((_, i) => i !== index);
@@ -24,11 +24,11 @@ const Home = () => {
 		
 		<div className="container mt-5">
 			
-			<h1 className="text-center mt-5">Todos</h1>
+			<h1 className="text-center display-1 mb-5">Todos</h1>
 
 			<div className="mx-auto col-6">
 		
-				<input id = "tareaNueva" type="email" className="form-control" placeholder=" ¿Que necesitas hacer?" onKeyDown={(e) => {
+				<input id = "tareaNueva" type="email" className="form-control mb-3" placeholder=" ¿Que necesitas hacer?" onKeyDown={(e) => {
       				if (e.key === "Enter") {
             			agregarTarea(document.getElementById("tareaNueva").value);
             			document.getElementById("tareaNueva").value = ""; }}}/ 
@@ -36,28 +36,37 @@ const Home = () => {
 
 				{/* <button onClick={()=> agregarTarea(document.getElementById("tareaNueva").value)}></button> */}
 				
-				<ul className="">
+				<ul className="list-group list-group-flush">
 										
 						{listaDeTareas.map((item, index) => {
 							
-							return (
+							return (<>
 
-								<li key = {index}>	
-									{item}	<i className="fa-solid fa-trash hideIcon" onClick={()=> eliminarTarea(index)}></i>
+								<li className= "list-group-item list-group-item-light" key = {index}>	
+									<div className="col-6">{item}	<i className="fa-solid fa-trash hideIcon" onClick={()=> eliminarTarea(index)}></i></div>
+									
 								</li>
+								
+								</>
 							)
 
 							})} 
+							<li className=
+				
+				{`mt-2 form-control p-3 text-primary-emphasis bg-primary-subtle border border-primary-subtle rounded-3 ${listaDeTareas.length === 0 ? "bg-success" : "bg-danger"}`}
+				
+				>
+					
+					{listaDeTareas.length === 0?"No hay tareas, añadir tareas":`Quedan ${listaDeTareas.length} tareas por completar`}</li>
 					
 				</ul>
+				
+							
+				
 		
-			</div>
-	
+		</div>		
+			
 		</div>
-		
-			
-			
-		
 	);
 };
 
