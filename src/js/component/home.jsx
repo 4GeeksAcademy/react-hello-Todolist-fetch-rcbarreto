c4b1052b7490598ba256975b3a3ed302a1a9a266
rcbarreto <69176511+rcbarreto@users.noreply.github.com>
2024-10-26 22:45:26 +0000

    pronto git add .!

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,37 +1,61 @@
 import React, {useState, useEffect} from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-import { isElementOfType } from "react-dom/test-utils";
 
-//create your first component
 const Home = () => {
 
-	let [listaDeTareas, setListaDeTareas] = useState(["Bañarse", "Limpiar","Cocinar","Aprender react"])
-	
-	const eliminarTarea = (index) => {
-        const nuevaLista = listaDeTareas.filter((_, i) => i !== index);
-        setListaDeTareas(nuevaLista); 
-    };
-	
-	const agregarTarea = (tarea) =>{
-		const nuevaLista = listaDeTareas.concat(tarea);
-		setListaDeTareas(nuevaLista); 
-	}
+	let [listaDeTareas, setListaDeTareas] = useState([])
 
-	useEffect(()=>{
-		const cargarTareas = async() => {
+	
+	const cargarTareas = async () => {
+		const url = "https://playground.4geeks.com/todo/users/rcbarreto";
+		const resp = await fetch(url);
+		const data = await resp.json();
+		setListaDeTareas(data.todos);
+};
 
-			const url = "https://playground.4geeks.com/todo/users/rcbarreto"
-			const resp = await fetch(url)
-			const data = await resp.json()
-			console.log(data)
+	useEffect(() => {
+		cargarTareas();
+	}, []);
+
+	async function agregarTareas(e) {
+
+		if (e.key === "Enter") {
+			
+			const url = "https://playground.4geeks.com/todo/todos/rcbarreto"
+			const resp = await fetch (url, {
+				method:"POST",
+				headers: {
+					'Content-Type' : 'application/json'
+				},
+				body: JSON.stringify({
+					label: document.getElementById("tareaNueva").value,
+					is_done: false
+				})
+			})
+			
+			if(resp.ok){
+				document.getElementById("tareaNueva").value = ""
+				cargarTareas()
+			}
+		
+	}}
+
+	async function eliminarTarea (idTarea) {
+	
+		const url = `https://playground.4geeks.com/todo/todos/${idTarea}`; 
+	   	const resp = await fetch(url, {
+				method: "DELETE",
+				headers: {
+					'Content-Type': 'application/json'
+				}
+			});
+	
+			if (resp.ok) {
+				
+				cargarTareas()
+		
 		}
-
-		cargarTareas()
-
-	},[])
-
+	}	
 
 	return (
 		
@@ -41,26 +65,23 @@ const Home = () => {
 
 			<div className="mx-auto col-6">
 		
-				<input id = "tareaNueva" type="email" className="form-control mb-3" placeholder=" ¿Que necesitas hacer?" onKeyDown={(e) => {
-      				if (e.key === "Enter") {
-            			agregarTarea(document.getElementById("tareaNueva").value);
-            			document.getElementById("tareaNueva").value = ""; }}}/ 
+				<input id = "tareaNueva" type="text" className="form-control mb-3" placeholder=" ¿Que necesitas hacer?" onKeyDown={(e) => {
+					agregarTareas(e)
+      				}}/
 				>
 
-				{/* <button onClick={()=> agregarTarea(document.getElementById("tareaNueva").value)}></button> */}
-				
+						
 				<ul className="list-group list-group-flush">
 										
-						{listaDeTareas.map((item, index) => {
+						{listaDeTareas.map((item) => {
 							
-							return (<>
+							return (
 
-								<li className= "list-group-item list-group-item-light" key = {index}>	
-									<div className="col-6">{item}	<i className="fa-solid fa-trash hideIcon" onClick={()=> eliminarTarea(index)}></i></div>
-									
+								<li className= "list-group-item list-group-item-light" key = {item.id}>	
+									<div className="col-6">{item.label}	<i className="fa-solid fa-trash hideIcon" 
+									onClick={()=> eliminarTarea(item.id)}></i></div>
 								</li>
 								
-								</>
 							)
 
 							})} 
